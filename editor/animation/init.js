54506e0122102bf29d7448cbd6cb7c4fa68e18eb
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-08-25 13:46:56 +1200

    js

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -40,10 +40,10 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210', 'snap.svg_030'],
             }
 
             //YOUR FUNCTION NAME
-            var fname = 'checkio';
+            var fname = 'index_power';
 
             var checkioInput = data.in;
-            var checkioInputStr = fname + '(' + JSON.stringify(checkioInput) + ')';
+            var checkioInputStr = fname + '(' + JSON.stringify(checkioInput[0]).replace("[", "(").replace("]", ")") + checkioInput[1] + ')';
 
             var failError = function (dError) {
                 $content.find('.call').html(checkioInputStr);
